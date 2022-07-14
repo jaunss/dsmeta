e6ecf33f0a8a876796f97bc0d2c977cd7d57cb7c
Joao Gabriel Maciel <joaogabriel36@hotmail.com>
2022-07-14 15:58:34 -0300

    Projeto criado.

---
+++ backend/src/main/java/com/joaogcm/dsmeta/DsmetaBackendApplication.java
@@ -0,0 +1,11 @@
+package com.joaogcm.dsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmetaBackendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DsmetaBackendApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/com/joaogcm/dsmeta/DsmetaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaogcm.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
